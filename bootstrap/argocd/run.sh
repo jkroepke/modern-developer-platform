2ed96655390f7ddb302a0d00ecd34ed9b0137a25
Jan-Otto Kröpke <mail@jkroepke.de>
2025-03-20 17:21:40 +0100

    Added initial bootstraping

--- bootstrap/argocd/run.sh
+++ bootstrap/argocd/run.sh
@@ -4,4 +4,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "Installing ArgoCD"
 
-helm upgrade -i argocd oci://ghcr.io/argoproj/argo-helm/argo-cd -n argocd --create-namespace --wait -f "${SCRIPT_DIR}/../../apps/argocd/values.yaml"
+helm upgrade -i argocd oci://ghcr.io/argoproj/argo-helm/argo-cd --version 7.8.13 -n argocd --create-namespace --wait -f "${SCRIPT_DIR}/../../apps/argocd/values.yaml"
+kubectl apply -f "${SCRIPT_DIR}/bootstrap.yaml"
